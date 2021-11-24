b3bbb335d1373af755920bda5d3337e6b9f3e659
soathekid <sebaoaki@gmail.com>
2021-11-24 18:26:22 -0300

    500 generations

--- src/GLOBAL.py
+++ src/GLOBAL.py
@@ -9,16 +9,16 @@ RECTANGLES_NUMBER = 20
 RESULTS_FOLDER = "results"
 TOURNAMENT_SIZE = 2
 POPULATION_SIZE = 50
-MAX_GENERATIONS = 1000
+MAX_GENERATIONS = 500
 MUTATION_PROBABILITY = .1
 CROSS_OVER_PROBABILITY = .65
 MAX_WIDTH = 100
 
 instances = {
         "spp9a": "spp9a.txt",
-        "spp9b": "spp9b.txt",
-        "spp10": "spp10.txt",
-        "spp11": "spp11.txt",
-        "spp12": "spp12.txt",
-        "spp13": "spp13.txt"
+        #"spp9b": "spp9b.txt",
+       # "spp10": "spp10.txt",
+       # "spp11": "spp11.txt",
+       # "spp12": "spp12.txt",
+       # "spp13": "spp13.txt"
     }
